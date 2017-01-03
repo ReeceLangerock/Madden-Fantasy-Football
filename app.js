8e16fcf540dcff75ae6eb6461d9e5510a03a2cc3
ReeceLangerock <reecelangerock@gmail.com>
2017-01-02 18:06:11 -0600

    test

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var admin = require("firebase-admin");
@@ -14,42 +12,50 @@ const serviceAccount = require("./madden-fantasy-football-firebase-adminsdk-mukt
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://madden-fantasy-football.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://madden-fantasy-football.firebaseio.com"
 });
 
 // Setup
 // Change the default port here if you want for local dev.
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
 
 app.get('/', function(req, res) {
-  //res.send("Ready to accept from CFM App");
-  return res.send('Madden Data');
+    //res.send("Ready to accept from CFM App");
+    return res.send('Madden Data');
 });
 
 // This accepts all posts requests!
 app.post('/*', function(req, res) {
-  const db = admin.database();
-  const ref = db.ref();
-  const dataRef= ref.child("data");
-  // Change what is set to the database here
-  // Rosters are in the body under rosterInfoList
-  const newDataRef = dataRef.push();
-  //newDataRef.set({
-  //  data: (req && req.body) || ''
-  //});
-
-  console.log(req.body.success);
-  console.log(req.body.teamStandingInfoList[0].teamName);
-  //for (var i in req.body.data.teamStandingInfoList){
-  //  tempData.name = req.body.teamStandingInfoList[i].teamName;
-  //}
-  res.send("Team Rankings: ");
+    const db = admin.database();
+    const ref = db.ref();
+    const dataRef = ref.child("data");
+    // Change what is set to the database here
+    // Rosters are in the body under rosterInfoList
+    const newDataRef = dataRef.push();
+    //newDataRef.set({
+    //  data: (req && req.body) || ''
+    //});
+    for (var i = 0; i < 32; i++) {
+        if (req.body.success)
+            console.log(req.body.teamStandingInfoList[i].teamName);
+    }
+    //for (var i in req.body.data.teamStandingInfoList){
+    //  tempData.name = req.body.teamStandingInfoList[i].teamName;
+    //}
+    res.send("Team Rankings: ");
 
 });
 
 
-app.listen(app.get('port'), function() { console.log('Madden Companion Exporter is running on port', app.get('port')) });
+app.listen(app.get('port'), function() {
+    console.log('Madden Companion Exporter is running on port', app.get('port'))
+});
