a54de0fcdb5a5e5197cedd292142a772308f2ca3
ReeceLangerock <reecelangerock@gmail.com>
2017-02-28 19:46:09 -0600

    check headers

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/test', function(req, res) {
 
 // This accepts all posts requests!
 app.post('/*', function(req, res) {
-    console.log(req);
+    console.log(req.headers);
     var data = req.body;
     db.collection('everything').insert({test : data});
 
