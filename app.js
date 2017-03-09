2f734aca211fed189e2a002872312710a5b40c08
ReeceLangerock <reecelangerock@gmail.com>
2017-03-08 18:14:37 -0600

    test

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post('/*', function(req, res) {
     //collection = String(collection);
     var data = req.body;
 
-    db.collection(collection).insert({documentName: data});
+    db.collection(collection).insert({[documentName]: data});
     //db.collection('everything').insert({test : data});
 
     /*
